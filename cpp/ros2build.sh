36f0f4bb96eb37581920ca5e9573c42bc4c9a21d
Nacy.zhan <zhanxiaonan8492@163.com>
2025-12-15 15:32:43 +0800

    Delete cpp directory

--- cpp/ros2build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-workspace=$(pwd)
-colcon build "$@"
-source install/setup.bash
-sudo cp ${workspace}/install/rh15_ctrl/lib/*.so /opt/ros/foxy/lib
-colcon build --packages-select rh15_ctrl
